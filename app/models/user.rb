26e3120e357446901be7d65e25848d981169867f
Leo Godin <leogodin217@gmail.com>
2010-01-17 09:26:55 -0700

    Added authentication logic to model.  Needed to add def password method for new user.  Not sure why since I already had def password=

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,26 @@ class User
 
   property :id,             Serial
   property :username,       String
-  property :password,       String
   property :email_address,  String
+  property :password_hash,  String, :length => 64
+  property :password_salt,  String
 
+  def authenticate(opts)
+   
+    puts "username sent is #{opts[:username]}, password sent is #{opts[:password]}"
+    puts "Self password_salt is #{self.password_salt}"
+    return false unless self.username == opts[:username]
+    Digest::SHA256.hexdigest(opts[:password] + self.password_salt) == self.password_hash
+    
+  end
 
+  def password=(password)
+    salt = rand
+    self.password_salt = salt
+    self.password_hash = Digest::SHA256.hexdigest(password + salt.to_s)
+  end
+
+  def password
+
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,19 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe User, "Create" do
   
-  it "authenticates with correct username and password" 
-#    @user = User.create :username => "myname", :password => "somepassword", :email_address => "me@you.com"
-#    @user.authenticate(:username => "myname", :password => "somepassword").should be_true
-#  end
+  it "authenticates with correct username and password"  do
+    @user = User.create :username => "myname", :password => "somepassword", :email_address => "me@you.com"
+    @user.authenticate({:username => "myname", :password => "somepassword"}).should be_true
+  end
+
+  it "fails with incorrect username " do
+    @user = User.create :username => "myname", :password => "somepassword", :email_address => "me@you.com"
+    @user.authenticate({:username => "myothername", :password => "somepassword"}).should be_false
+  end
+
+  it "fails with incorrect password" do
+    @user = User.create :username => "myname", :password => "somepassword", :email_address => "me@you.com"
+    @user.authenticate({:username => "myname", :password => "someotherpassword"}).should be_false
+  end
+    
 end
